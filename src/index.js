1df39ef102a0aee13060db71e2f3a4812f5dcf2e
TuanAnh15092001 <nguyentuan1592001@gmail.com>
2025-11-25 19:12:29 +0700

    SCSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 // import express from 'express';
+//khai báo các biến và hằng số, sử dụng require thay vì import, const thay vì let hoặc var
 const express = require('express')
 const handlebars = require('express-handlebars');
 const app = express()
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+
+//Static files , lấy file tĩnh từ thư mục Content, chỉ cần gõ localhost:3000/tenfile*(img/logo.jpeg )
+app.use(express.static(path.join(__dirname, 'Content')));
 
 //HTTP request logger 
 const morgan = require('morgan');
